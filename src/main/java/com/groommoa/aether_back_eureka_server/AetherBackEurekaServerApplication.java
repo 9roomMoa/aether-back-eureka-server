4e22dca838a1b207ca1f5f1034d06960af42e30c
EveryPine <starcraft819@gmail.com>
2025-02-22 16:20:36 +0900

    fix: 시간대 설정 변경
    - 시간대를 UTC +9 (KST)로 변경

--- src/main/java/com/groommoa/aether_back_eureka_server/AetherBackEurekaServerApplication.java
+++ src/main/java/com/groommoa/aether_back_eureka_server/AetherBackEurekaServerApplication.java
@@ -1,12 +1,21 @@
 package com.groommoa.aether_back_eureka_server;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class AetherBackEurekaServerApplication {
+
+	private final String timeZone = "Asia/Seoul";
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AetherBackEurekaServerApplication.class, args);
